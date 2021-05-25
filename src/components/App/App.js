2677a2bebb37fc9a4e24c20d87c42f3af23777b3
Lucas de Morais <lucasmoraismt@gmail.com>
2021-05-24 22:24:00 -0300

    start of project

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserContext from "../../contexts/UserContexts";
+
+import "../../css/reset.css";
+import "../../css/styles.css";
+
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
+import Timeline from "../Timeline/Timeline";
+
+export default function App() {
+  const [user, setUser] = useState();
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/sign-up" exact>
+            <SignUp />
+          </Route>
+          <Route path="/timeline" exact>
+            <Timeline />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
