f7c939cd530545eb4043c72d2584b27d89a91ae1
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-05-26 20:50:39 -0300

    Delete post works on /user/id page

--- src/components/UserPage.js/UserPage.js
+++ src/components/UserPage.js/UserPage.js
@@ -69,7 +69,7 @@ export default function UserPage() {
           ) : posts.length === 0 ? (
             <p className="warning">Nenhum post encontrado</p>
           ) : (
-            <PostsList posts={posts} />
+            <PostsList posts={posts} reload={getPosts} />
           )}
         </div>
         <div className="page-right">
