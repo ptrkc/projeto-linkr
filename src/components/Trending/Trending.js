c2ae29ee6a94bdfe4809f019a84c70d8a00073f2
Lucas de Morais <lucasmoraismt@gmail.com>
2021-05-27 23:00:00 -0300

    trending layout minor fix

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,107 +1,117 @@
-import axios from 'axios';
-import {useEffect, useContext, useState} from 'react';
-import styled from 'styled-components';
-import {useHistory} from 'react-router-dom';
+import axios from "axios";
+import { useEffect, useContext, useState } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 import UserContext from "../../contexts/UserContexts";
 
-export default function Trending(){
-    
-    const {user} = useContext(UserContext);
-    const [trendingList, setTrendingList] = useState([]);
-    const [requestError, setRequestError] = useState();
-    const [loading, setLoading] = useState(false);
-    let history = useHistory();
+export default function Trending() {
+  const { user } = useContext(UserContext);
+  const [trendingList, setTrendingList] = useState([]);
+  const [requestError, setRequestError] = useState();
+  const [loading, setLoading] = useState(false);
+  let history = useHistory();
 
-    useEffect(() => {
-        if(user){
-            setLoading(true);
-            const config = {
-                headers: {
-                    "Authorization": `Bearer ${user.token}`
-                }
-            }
-            const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/trending",config);
+  useEffect(() => {
+    if (user) {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const request = axios.get(
+        "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/trending",
+        config
+      );
 
-            request.then(response=>{
-                setTrendingList(response.data.hashtags);
-                setLoading(false);
-            });
-            request.catch(error=>{
-                handler(error);
-                setLoading(false);
-            });
-        }
-	},[user]);
-
-    function goToHashtag(name){
-        history.push("/hashtag/"+name);
+      request.then((response) => {
+        setTrendingList(response.data.hashtags);
+        setLoading(false);
+      });
+      request.catch((error) => {
+        handler(error);
+        setLoading(false);
+      });
     }
+  }, [user]);
 
-    function handler(error){
-        setRequestError(error.response.statusText);  
-    }
+  function goToHashtag(name) {
+    history.push("/hashtag/" + name);
+  }
 
-    function refresh(){
-        setLoading(true);
-        const config = {
-            headers: {
-                "Authorization": `Bearer ${user?user.token:""}`
-            }
-        }
-		const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/trending",config);
-        
-        request.then(response=>{
-            setTrendingList(response.data.hashtags);
-            setRequestError();
-            setLoading(false);
-        });
-        request.catch(error=>{
-            handler(error);
-            setLoading(false);
-        });
-    }
+  function handler(error) {
+    setRequestError(error.response.statusText);
+  }
 
-    return(
-        <Container>
-            <Title>trending</Title>
-            <ContainerList>
-                {requestError?
-                    <div onClick={refresh}>{requestError}</div>
-                :
-                    (loading?
-                        "carregando..."
-                    : 
-                        (trendingList.length>0?trendingList.map(item=><div key={item.id} onClick={()=>goToHashtag(item.name)}># {item.name}</div>):<></>)                   
-                    )
-                    
-                }
-            </ContainerList>
-        </Container>);
+  function refresh() {
+    setLoading(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user ? user.token : ""}`,
+      },
+    };
+    const request = axios.get(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/trending",
+      config
+    );
+
+    request.then((response) => {
+      setTrendingList(response.data.hashtags);
+      setRequestError();
+      setLoading(false);
+    });
+    request.catch((error) => {
+      handler(error);
+      setLoading(false);
+    });
+  }
+
+  return (
+    <Container>
+      <Title>trending</Title>
+      <ContainerList>
+        {requestError ? (
+          <div onClick={refresh}>Try again</div>
+        ) : loading ? (
+          "carregando..."
+        ) : trendingList.length > 0 ? (
+          trendingList.map((item) => (
+            <a key={item.id} href={`/hashtag/${item.name}`}>
+              # {item.name}
+            </a>
+          ))
+        ) : (
+          <></>
+        )}
+      </ContainerList>
+    </Container>
+  );
 }
 
-const Container = styled.div`
-    
-`;
+const Container = styled.div``;
 
 const Title = styled.div`
-    font-family: 'Oswald', sans-serif;
-    font-weight: bold;
-    font-size: 27px;
-    line-height: 40px;
-    display: flex;
-    border-bottom: 1px solid #484848;
-    padding: 16px;
+  font-family: "Oswald", sans-serif;
+  font-weight: bold;
+  font-size: 27px;
+  line-height: 40px;
+  display: flex;
+  border-bottom: 1px solid #484848;
+  padding: 16px;
 `;
 
 const ContainerList = styled.div`
-    font-family: 'Lato', sans-serif;
-    font-weight: bold;
-    font-size: 19px;
-    line-height: 23px;
-    letter-spacing: 0.05em;
-    padding: 26px 16px;
-    
-    div {
-        margin-bottom: 5px;
-    }
+  font-family: "Lato", sans-serif;
+  font-weight: bold;
+  font-size: 19px;
+  line-height: 23px;
+  letter-spacing: 0.05em;
+  padding: 26px 16px;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    margin-bottom: 5px;
+    width: fit-content;
+  }
 `;
