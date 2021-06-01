ae018c1b8db52fd3e161d962191490d883bf7a1c
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2021-06-01 14:54:13 -0300

    Trying to deploy on Vercel.

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -68,6 +68,7 @@ export default function SignUp() {
     }
   }
   function validateEmail(email) {
+    // eslint-disable-next-line
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
